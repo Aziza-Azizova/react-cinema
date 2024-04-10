32ce0eef12641588a0df3c7b2acb1c0bcb19574d
Aziza-Azizova <aziza.azizova01@gmail.com>
2024-04-10 22:28:52 +0500

    Added active circle to links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,32 @@
-import React from 'react'
+import { useState } from 'react'
 import logoImg from "@i/logo.svg";
 import { router } from '../router';
 import { NavLink } from "react-router-dom";
+
 function Header() {
-  return (
-    <header className='header'>
-        <div className="container">
-            <div className="header__nav">
-                <a href="" className="logo"><img src={logoImg} alt="" /></a>
-                <ul className="header__list">
-                    {
-                        router.map((route,index)=>{
-                            return <li key={index}>
-                                <NavLink to={route.path} className="header__link">
-                                    {route.name}
-                                </NavLink>
+    const [active, setActive] = useState(0);
+
+    return (
+        <header className='header'>
+            <div className="container">
+                <div className="header__nav">
+                    <a href="" className="logo"><img src={logoImg} alt="" /></a>
+                    <ul className="header__list">
+                        {
+                            router.map((route, index) => {
+                                return <li key={index}>
+                                    <NavLink to={route.path} className="header__link" onClick={() => setActive(index)}>
+                                        {route.name}
+                                    </NavLink>
                                 </li>
-                        })
-                    }
-                    
-                </ul>
+                            })
+                        }
+                        <span className={`circle ${active === 0 ? 'active' : active === 1 ? 'move2' : active === 2 ? 'move3' : active === 3 ? 'move4' : ''}`}></span>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Header
